3c73c36c94a5809e6ceb14d840664d8f2f0e22c7
jvbs <jvbs98@hotmail.com>
2021-06-19 01:18:58 -0300

    criado crud de solicitacao de demonstracao

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useRef } from "react";
+import { useField } from "@unform/core";
 import { TextField } from "@material-ui/core";
 
-const Input = ({ label, testid }) => {
+const Input = ({ label, testid, name, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+
+  // registrando alterações no input
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
+
   return (
-    <TextField style={{ marginTop: "3%", marginBottom: "3%" }} testid={testid} label={label} />
+    <>
+      <TextField
+        style={{ marginTop: "3%", marginBottom: "3%" }}
+        testid={testid}
+        label={label}
+        name={name}
+        inputRef={inputRef}
+        {...rest}
+      />
+
+      {error && (
+        <span style={{ color: "#f00", fontSize: "10pt" }}>{error}</span>
+      )}
+    </>
   );
 };
 
