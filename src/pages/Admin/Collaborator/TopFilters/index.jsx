a026f9b8770fc39cac19af4e6173f5ef38cd4538
jvbs <jvbs98@hotmail.com>
2021-07-03 20:14:06 -0300

    solicitacao redirect login

--- src/pages/Admin/Collaborator/TopFilters/index.jsx
+++ src/pages/Admin/Collaborator/TopFilters/index.jsx
@@ -1,52 +1,53 @@
 import { useRef } from "react";
-import { MenuItem } from "@material-ui/core";
 import { Col, FormGroup, Row } from "reactstrap";
-import { FaCamera } from "react-icons/fa";
 import { Form } from "@unform/web";
-import * as Yup from "yup";
 import { FaSearch } from "react-icons/fa";
 
 import Input from "../../../../components/Input";
-import Select from "../../../../components/Select";
 
 import styles from "./styles.module.css";
-import userPhoto from "../../../../assets/images/evee.png";
 import Button from "../../../../components/Button";
-
+import history from "../../../../utils/history";
 
 const TopFilters = () => {
   const formRef = useRef(null);
   return (
     <Form ref={formRef} style={{ marginBottom: "3vh" }}>
-    <Row>
-  
-       <Col lg="7" style={{ backgroundColor: "" }}>
+      <Row>
+        <Col lg="8" style={{ backgroundColor: "" }}>
           <FormGroup>
-              <Input label="Informe o nome ou e-mail do colaborador" name="pesquisa" testid="fieldPesquisarColaborador" style={{ marginTop: "0", marginBottom: "0" }}/>
+            <Input
+              label="Informe o nome ou e-mail do colaborador"
+              name="pesquisa"
+              testid="fieldPesquisarColaborador"
+              style={{ marginTop: "0", marginBottom: "0" }}
+            />
           </FormGroup>
-       </Col>
+        </Col>
 
-       <Col lg="2" style={{ backgroundColor: "" }}>
-        <button type="button" data-testid="btnPesquisar" className={styles.btnSearch}>
-              <FaSearch
-                fontSize="1.3vw"
-                style={{ color: "var(--yellow-gold)", opacity: "80%" }}
-              />
-        </button>
-       </Col>
+        {/* <Col lg="2" style={{ backgroundColor: "" }}>
+          <button
+            type="button"
+            data-testid="btnPesquisar"
+            className={styles.btnSearch}
+          >
+            <FaSearch
+              fontSize="1.3vw"
+              style={{ color: "var(--yellow-gold)", opacity: "80%" }}
+            />
+          </button>
+        </Col> */}
 
-       <Col lg="3" style={{ backgroundColor: "" }} >
-        <Button
-                    text="Cadastrar"
-                    onClick="  "
-                    style={{ width: "100%"}}
+        <Col lg={{ size: 3, offset: 1 }} style={{ backgroundColor: "" }}>
+          <Button
+            text="Cadastrar"
+            onClick={() => history.push("/admin/create-collaborator")}
+            style={{ width: "100%" }}
           />
-       </Col>
-
-    </Row>
+        </Col>
+      </Row>
     </Form>
   );
-  
 };
 
 export default TopFilters;
