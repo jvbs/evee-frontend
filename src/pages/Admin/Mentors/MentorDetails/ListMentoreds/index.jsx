6057cbb084b265af9f5883455e370a17c0987d01
Pedro Henrique Gonzaga <henrique.gonzaga@live.com>
2021-07-04 23:23:10 -0300

    inclui detalhes do mentor

--- src/pages/Admin/Mentors/MentorDetails/ListMentoreds/index.jsx
+++ src/pages/Admin/Mentors/MentorDetails/ListMentoreds/index.jsx
@@ -6,7 +6,7 @@ import history from "../../../../../utils/history";
 import UserBox from "../../../../../components/UserBox";
 import ContentSearchReturn from "../../../../../components/ContentSearchReturn";
 
-const ListMentors = ({ users, filter }) => {
+const ListMentoreds = ({ users, filter }) => {
   const filteredUsers = useMemo(() => {
     return users.filter((user) => {
       if (!filter) return true;
@@ -22,7 +22,7 @@ const ListMentors = ({ users, filter }) => {
 
   return (
     <>
-      <ContentSearchReturn section="Mentores" qtdUsers={filteredUsers.length} />
+      <ContentSearchReturn section="Aprendizes" qtdUsers={filteredUsers.length} />
       <Row>
         {filteredUsers.length > 0 ? (
           filteredUsers
@@ -46,14 +46,37 @@ const ListMentors = ({ users, filter }) => {
         ) : (
           <div className={styles.BoxTextGroup}>
             <p className={styles.BoxTextOne}>
-              Ops! nenhum resultado encontrado...
+            Ops! Nenhum <strong>Aprendiz</strong> encontrado...
             </p>
-            <p className={styles.BoxTextTwo}>O que eu faço?</p>
-            <p className={styles.BoxTextList}>
-              Verifique se possui mentores cadastrados.
-            </p>
-            <p className={styles.BoxTextList}>
-              Verifique os termos digitados ou os filtros selecionados.
+          </div>
+        )}
+      </Row>
+      
+      <ContentSearchReturn section="Estagiarios" qtdUsers={filteredUsers.length} />
+      <Row>
+        {filteredUsers.length > 0 ? (
+          filteredUsers
+            .filter((user) => {
+              if (!filter) return true;
+              if (user.nome.includes(filter) || user.email.includes(filter)) {
+                return true;
+              }
+            })
+            .map((user, index) => {
+              return (
+                <UserBox
+                  key={index}
+                  nome={splitName(user.nome)}
+                  cargo={user.nome_cargo}
+                  departamento={user.nome_departamento}
+                  onClick={() => history.push("/admin/create-collaborator")}
+                />
+              );
+            })
+        ) : (
+          <div className={styles.BoxTextGroup}>
+            <p className={styles.BoxTextOne}>
+              Ops! Nenhum <strong>Estagiário</strong> encontrado...
             </p>
           </div>
         )}
@@ -62,4 +85,4 @@ const ListMentors = ({ users, filter }) => {
   );
 };
 
-export default ListMentors;
+export default ListMentoreds;
--- src/pages/Admin/Mentors/MentorDetails/index.jsx
+++ src/pages/Admin/Mentors/MentorDetails/index.jsx
@@ -1,17 +1,61 @@
+import { Col, FormGroup, Row } from "reactstrap";
+import { Form } from "@unform/web";
+import { useEffect, useRef, useState } from "react";
+
 import BodyContent from "../../../../components/BodyContent";
 import Layout from "../../../../components/Layout";
+import Input from "../../../../components/Input";
+import Button from "../../../../components/Button";
+import history from "../../../../utils/history";
+import ListMentoreds from "./ListMentoreds";
 
 import styles from "./styles.module.css";
+import { api } from "../../../../services/api";
 
-const MentorDetails = () => (
+const MentorDetails = () => {
+  const formRef = useRef(null);
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await api.get("/colaborador/mentorados");
+
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+  return (
   <Layout>
     <BodyContent
-      header="Gerenciamento de Mentores"
+      header="Painel do Colaborador: Celso Donato"
       breadcrumb="Home > Mentores > Mentor"
     >
-      <p>Mentor details</p>
+      <Row>
+          <Col lg="8" style={{ backgroundColor: "" }}>
+            <Form
+              ref={formRef}
+              style={{ marginBottom: "3vh" }}
+              onSubmit={() => {}}
+            >
+              <FormGroup>
+                <Input
+                  label="Informe o nome ou e-mail do mentorado"
+                  name="pesquisa"
+                  testid="fieldPesquisarColaborador"
+                  style={{ marginTop: "0", marginBottom: "0" }}
+                  onChange={(e) => setFilter(e.target.value)}
+                />
+              </FormGroup>
+            </Form>
+          </Col>
+
+        </Row>
+        <ListMentoreds users={users} filter={filter} />
     </BodyContent>
   </Layout>
 );
+};
 
 export default MentorDetails;
