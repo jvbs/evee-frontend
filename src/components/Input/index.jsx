dd761a7d7dc1cd1982c872f584d8fd6cceae59ba
jvbs <jvbs98@hotmail.com>
2021-07-15 23:38:41 -0300

    input masking sample

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import { useField } from "@unform/core";
 import { TextField } from "@material-ui/core";
+import ReactInputMask from "react-input-mask";
 const Input = ({ label, testid, name, ...rest }) => {
   const inputRef = useRef(null);
   // eslint-disable-next-line
@@ -22,15 +23,19 @@ const Input = ({ label, testid, name, ...rest }) => {
 
   return (
     <>
-      <TextField
-        style={{ marginTop: "3%", marginBottom: "3%" }}
-        testid={testid}
-        label={label}
-        name={name}
-        inputRef={inputRef}
-        defaultValue={unformDefaultValue}
-        {...rest}
-      />
+      <ReactInputMask maskChar="_" {...rest}>
+        {() => (
+          <TextField
+            style={{ marginTop: "3%", marginBottom: "3%" }}
+            testid={testid}
+            label={label}
+            name={name}
+            inputRef={inputRef}
+            defaultValue={unformDefaultValue}
+            {...rest}
+          />
+        )}
+      </ReactInputMask>
 
       {error && (
         <span style={{ color: "#f00", fontSize: "10pt" }}>{error}</span>
