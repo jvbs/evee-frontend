a8430d2a733bceda2eab4991b7212a8d2110da75
Pedro Henrique Gonzaga <henrique.gonzaga@live.com>
2021-08-01 02:00:47 -0300

    update img default

--- src/pages/Admin/CreateCollaborator/CreateForm/index.jsx
+++ src/pages/Admin/CreateCollaborator/CreateForm/index.jsx
@@ -15,7 +15,7 @@ import Select from "../../../../components/Select";
 import Button from "../../../../components/Button";
 
 import history from "../../../../utils/history";
-import userPhoto from "../../../../assets/images/evee.png";
+import userPhoto from "../../../../assets/images/avatar2.png";
 import styles from "./styles.module.css";
 import "react-toastify/dist/ReactToastify.min.css";
 
